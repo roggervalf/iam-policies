2c2c5df7125f9d033e30a4a9c9646ae5629aa53a
Rogger794 <rogger.valverde@uni.pe>
2019-12-19 17:32:25 -0500

    chore: adding types definitions and interfaces

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,12 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 
-import typescript from "rollup-plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 
 import pkg from "./package.json";
 
 export default {
-  input: "src/main.ts",
+  input: "./main.ts",
   output: [
     {
       file: pkg.main,
--- src/main.ts
+++
@@ -1,2 +0,0 @@
-export * from './Role'
-export * from './Statement'

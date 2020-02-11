15011e444b68f17eb3a606a1f10e5c23e84d1ffd
Luis <f1729@users.noreply.github.com>
2020-02-11 15:00:51 -0500

    fix: add '?' missing in regex

--- src/Statement.ts
+++ src/Statement.ts
@@ -160,7 +160,7 @@ export class Matcher {
 
       if (c === '*'){
         this.hasSpecialCharacter = true;
-        re += '[^/]*';
+        re += '[^/]*?';
       } else{
         re += c;
       }          

2b829de1baa2dc3aa774688654a0f8c9065d79c4
rogger andré valverde flores <rogger.valverde@uni.pe>
2020-03-29 22:08:05 -0500

    fix(rollup): deleting unused external-helpers plugin

--- rollup.config.js
+++ rollup.config.js
@@ -28,8 +28,7 @@ export default {
     }),
     external(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers']
+      exclude: 'node_modules/**'
     }),
     resolve(),
     commonjs()
